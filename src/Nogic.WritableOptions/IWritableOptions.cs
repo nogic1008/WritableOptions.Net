123b7769e8cd3d0c0193cefadbf89f4aa8c9ee35
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-03-03 16:16:00 +0900

    docs: bundle XML document on Release build (#13)
    * docs: add XML documentation comment
    * build: generate XML document on release build

--- src/Nogic.WritableOptions/IWritableOptions.cs
+++ src/Nogic.WritableOptions/IWritableOptions.cs
@@ -2,8 +2,17 @@ using Microsoft.Extensions.Options;
 
 namespace Nogic.WritableOptions
 {
+    /// <summary>
+    /// Used to access or update the value of TOptions.
+    /// </summary>
+    /// <typeparam name="TOptions">Options type.</typeparam>
     public interface IWritableOptions<TOptions> : IOptionsSnapshot<TOptions> where TOptions : class, new()
     {
+        /// <summary>
+        /// Update current <typeparamref name="TOptions"/> value.
+        /// </summary>
+        /// <param name="changedValue">value that you want to update.</param>
+        /// <param name="reload">Reload all configuration immediately or not.</param>
         void Update(TOptions changedValue, bool reload = false);
     }
 }
--- src/Nogic.WritableOptions/ServiceCollectionExtension.cs
+++ src/Nogic.WritableOptions/ServiceCollectionExtension.cs
@@ -7,33 +7,33 @@ using Microsoft.Extensions.Options;
 
 namespace Nogic.WritableOptions
 {
+    /// <summary>
+    /// Extension methods for adding configuration related writable options services to the DI container.
+    /// </summary>
     public static class ServiceCollectionExtension
     {
-        public static void ConfigureWritable<T>(
+        /// <summary>
+        /// Registers a writable configuration instance which <typeparamref name="TOptions"/> will bind against.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="section">The configuration being bound.</param>
+        /// <param name="file">Setting JSON file name. (should be placed in content-root folder or current folder)</param>
+        public static void ConfigureWritable<TOptions>(
             this IServiceCollection services,
             IConfigurationSection section,
-            string file = "appsettings.json") where T : class, new()
+            string file = "appsettings.json") where TOptions : class, new()
         {
-            services.Configure<T>(section);
-            services.AddTransient<IWritableOptions<T>>(provider =>
+            services.Configure<TOptions>(section);
+            services.AddTransient<IWritableOptions<TOptions>>(provider =>
             {
-                string jsonFilePath;
-
                 var environment = provider.GetService<IHostEnvironment>();
-                if (environment is not null)
-                {
-                    var fileProvider = environment.ContentRootFileProvider;
-                    var fileInfo = fileProvider.GetFileInfo(file);
-                    jsonFilePath = fileInfo.PhysicalPath;
-                }
-                else
-                {
-                    jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file);
-                }
+                string jsonFilePath = environment?.ContentRootFileProvider.GetFileInfo(file).PhysicalPath
+                    ?? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file);
 
                 var configuration = provider.GetService<IConfigurationRoot>();
-                var options = provider.GetRequiredService<IOptionsMonitor<T>>();
-                return new JsonWritableOptions<T>(jsonFilePath, section.Key, options, configuration);
+                var options = provider.GetRequiredService<IOptionsMonitor<TOptions>>();
+                return new JsonWritableOptions<TOptions>(jsonFilePath, section.Key, options, configuration);
             });
         }
     }
