a1b783c9eee574af42b07a6331dc0a82d074d9c6
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-07-06 16:20:38 +0900

    test: add Reload() call test

--- test/Nogic.WritableOptions.Tests/JsonWritableOptions.Test.cs
+++ test/Nogic.WritableOptions.Tests/JsonWritableOptions.Test.cs
@@ -34,7 +34,7 @@ namespace Nogic.WritableOptions.Tests
         /// <summary>
         /// <see cref="JsonWritableOptions{TOptions}.Value"/> returns TOptions via <see cref="IOptionsMonitor{TOptions}"/>.
         /// </summary>
-        [Fact(DisplayName = "JsonWritableOptions<TOptions>.Value returns TOptions")]
+        [Fact(DisplayName = ".Value returns TOptions via IOptionsMonitor<TOptions>")]
         public void Value_Returns_T()
         {
             // Arrange
@@ -50,9 +50,9 @@ namespace Nogic.WritableOptions.Tests
         }
 
         /// <summary>
-        /// <see cref="JsonWritableOptions{TOptions}.Get(string)"/> returns TOptions via <see cref="IOptionsMonitor{TOptions}"/>.
+        /// <see cref="JsonWritableOptions{TOptions}.Get"/> returns TOptions via <see cref="IOptionsMonitor{TOptions}"/>.
         /// </summary>
-        [Fact(DisplayName = "JsonWritableOptions<TOptions>.Get(string) returns TOptions")]
+        [Fact(DisplayName = ".Get(string) returns TOptions via IOptionsMonitor<TOptions>")]
         public void Get_Returns_T()
         {
             // Arrange
@@ -72,10 +72,10 @@ namespace Nogic.WritableOptions.Tests
         }
 
         /// <summary>
-        /// <see cref="JsonWritableOptions{TOptions}.Update(TOptions, bool)"/> writes expected JSON.
+        /// <see cref="JsonWritableOptions{TOptions}.Update"/> writes expected JSON.
         /// </summary>
         /// <param name="fileText">Current JSON text</param>
-        [Theory(DisplayName = "JsonWritableOptions<TOptions>.Update(TOptions, bool) writes expected JSON")]
+        [Theory(DisplayName = ".Update(TOptions) writes expected JSON")]
         [InlineData("{}")]
         [InlineData("{\"" + nameof(SampleOption) + "\":{}}")]
         [InlineData("{\"" + nameof(SampleOption) + "\":{\"LastLaunchedAt\":\"2020-10-01T00:00:00\",\"Interval\":1000,\"ConnectionString\":\"bar\"}}")]
@@ -84,6 +84,94 @@ namespace Nogic.WritableOptions.Tests
             // Setup
             string tempFilePath = Path.GetTempFileName();
             File.AppendAllText(tempFilePath, fileText);
+            string tempFileName = Path.GetFileName(tempFilePath);
+
+            try
+            {
+                // Arrange
+                var configStub = new Mock<IConfigurationRoot>();
+
+                var sut = new JsonWritableOptions<SampleOption>(tempFilePath, nameof(SampleOption), null!, configStub.Object);
+
+                // Act
+                sut.Update(new()
+                {
+                    LastLaunchedAt = new(2020, 12, 1),
+                    Interval = 5000,
+                    ConnectionString = "foo",
+                });
+
+                // Assert
+                string newLine = Environment.NewLine;
+                string jsonString = File.ReadAllText(tempFilePath);
+                jsonString.Should().Be("{" + newLine
+                    + "  \"" + nameof(SampleOption) + "\": {" + newLine
+                    + "    \"LastLaunchedAt\": \"2020-12-01T00:00:00\"," + newLine
+                    + "    \"Interval\": 5000," + newLine
+                    + "    \"ConnectionString\": \"foo\"" + newLine
+                    + "  }" + newLine
+                    + "}"
+                );
+                configStub.Verify(m => m.Reload(), Times.Never());
+            }
+            finally
+            {
+                // Teardown
+                if (File.Exists(tempFilePath))
+                    File.Delete(tempFilePath);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="JsonWritableOptions{TOptions}.Update(TOptions, bool)"/> does not changes other JSON section.
+        /// </summary>
+        /// <param name="fileText">Current JSON text</param>
+        [Theory(DisplayName = ".Update(TOptions) does not changes other JSON section")]
+        [InlineData("{\"fooOption\":{}}")]
+        [InlineData("{\"fooOption\":{},\"" + nameof(SampleOption) + "\":{}}")]
+        [InlineData("{\"fooOption\":{},\"" + nameof(SampleOption) + "\":{\"LastLaunchedAt\":\"2020-10-01T00:00:00\",\"Interval\":1000,\"ConnectionString\":\"bar\"}}")]
+        public void Update_DoesNot_Changes_Other_Section(string fileText)
+        {
+            // Setup
+            string tempFilePath = Path.GetTempFileName();
+            File.AppendAllText(tempFilePath, fileText);
+            string tempFileName = Path.GetFileName(tempFilePath);
+
+            try
+            {
+                // Arrange
+                var sut = new JsonWritableOptions<SampleOption>(tempFilePath, nameof(SampleOption), null!);
+
+                // Act
+                sut.Update(new()
+                {
+                    LastLaunchedAt = new(2020, 12, 1),
+                    Interval = 5000,
+                    ConnectionString = "foo",
+                }, true);
+
+                // Assert
+                string newLine = Environment.NewLine;
+                string jsonString = File.ReadAllText(tempFilePath);
+                jsonString.Should().Contain("\"fooOption\": {},");
+            }
+            finally
+            {
+                // Teardown
+                if (File.Exists(tempFilePath))
+                    File.Delete(tempFilePath);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="JsonWritableOptions{TOptions}.Update"/> calls <see cref="IConfigurationRoot.Reload"/>.
+        /// </summary>
+        [Fact(DisplayName = ".Update(TOptions, true) calls IConfigurationRoot.Reload()")]
+        public void Update_Calls_Reload()
+        {
+            // Setup
+            string tempFilePath = Path.GetTempFileName();
+            File.AppendAllText(tempFilePath, "{}");
             string tempFileName = Path.GetFileName(tempFilePath);
 
             try
@@ -113,50 +201,6 @@ namespace Nogic.WritableOptions.Tests
                     + "}"
                 );
                 configStub.Verify(m => m.Reload(), Times.Once());
-            }
-            finally
-            {
-                // Teardown
-                if (File.Exists(tempFilePath))
-                    File.Delete(tempFilePath);
-            }
-        }
-
-        /// <summary>
-        /// <see cref="JsonWritableOptions{TOptions}.Update(TOptions, bool)"/> does not changes other section.
-        /// </summary>
-        /// <param name="fileText">Current JSON text</param>
-        [Theory(DisplayName = "JsonWritableOptions<TOptions>.Update(TOptions, bool) does not changes other section")]
-        [InlineData("{\"fooOption\":{}}")]
-        [InlineData("{\"fooOption\":{},\"" + nameof(SampleOption) + "\":{}}")]
-        [InlineData("{\"fooOption\":{},\"" + nameof(SampleOption) + "\":{\"LastLaunchedAt\":\"2020-10-01T00:00:00\",\"Interval\":1000,\"ConnectionString\":\"bar\"}}")]
-        public void Update_DoesNot_Changes_Other_Section(string fileText)
-        {
-            // Setup
-            string tempFilePath = Path.GetTempFileName();
-            File.AppendAllText(tempFilePath, fileText);
-            string tempFileName = Path.GetFileName(tempFilePath);
-
-            try
-            {
-                // Arrange
-                var configStub = new Mock<IConfigurationRoot>();
-
-                var sut = new JsonWritableOptions<SampleOption>(tempFilePath, nameof(SampleOption), null!, configStub.Object);
-
-                // Act
-                sut.Update(new()
-                {
-                    LastLaunchedAt = new(2020, 12, 1),
-                    Interval = 5000,
-                    ConnectionString = "foo",
-                });
-
-                // Assert
-                string newLine = Environment.NewLine;
-                string jsonString = File.ReadAllText(tempFilePath);
-                jsonString.Should().Contain("\"fooOption\": {},");
-                configStub.Verify(m => m.Reload(), Times.Never());
             }
             finally
             {
