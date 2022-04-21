634ad6278f1e472bb5188dd35a66f2c16d4f8d47
Pavel Anpin <pavel@apes.ge>
2022-04-21 21:13:44 +0400

    Added extension for providing static file path Useful for custom hosts where IHostEnvironment is not implemented

--- src/Nogic.WritableOptions/ServiceCollectionExtension.cs
+++ src/Nogic.WritableOptions/ServiceCollectionExtension.cs
@@ -32,4 +32,24 @@ public static class ServiceCollectionExtension
                 var options = provider.GetRequiredService<IOptionsMonitor<TOptions>>();
                 return new JsonWritableOptions<TOptions>(jsonFilePath, section.Key, options, configuration);
             });
+    /// <summary>
+    /// Registers a writable configuration instance which <typeparamref name="TOptions"/> will bind against.
+    /// </summary>
+    /// <typeparam name="TOptions">The type of options being configured.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <param name="section">The configuration being bound.</param>
+    /// <param name="file">Setting JSON file name. (should be placed in content-root folder or current folder)</param>
+    public static void ConfigureWritable<TOptions>(
+        this IServiceCollection services,
+        IConfigurationSection section, string filePath,
+        string file = "appsettings.json") where TOptions : class, new()
+        => services.Configure<TOptions>(section)
+            .AddTransient<IWritableOptions<TOptions>>(provider =>
+            {
+                var environment = provider.GetService<IHostEnvironment>();
+                string jsonFilePath = Path.Combine(filePath, file);
+                var configuration = provider.GetService<IConfigurationRoot>();
+                var options = provider.GetRequiredService<IOptionsMonitor<TOptions>>();
+                return new JsonWritableOptions<TOptions>(jsonFilePath, section.Key, options, configuration);
+            });
 }
