0c999b71128b67bfcc590a5123b7a18e8bba009b
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-04-25 15:39:55 +0900

    style: add null forgiving operator

--- test/Nogic.WritableOptions.Tests/ServiceCollectionExtension.Test.cs
+++ test/Nogic.WritableOptions.Tests/ServiceCollectionExtension.Test.cs
@@ -26,15 +26,16 @@ public sealed class ServiceCollectionExtensionTest
             .And.BeOfType<JsonWritableOptions<SampleOption>>()
             .And.NotBe(otherOption);
     }
+
     [Fact]
-    public void ConfigureWritable_Calls_AddTransient_With_Explicit_Path()
+    public void ConfigureWritableWithExplicitPath_Calls_AddTransient()
     {
         var configuration = new ConfigurationBuilder()
             .AddInMemoryCollection()
             .Build();
         var service = new ServiceCollection();
 
-        service.ConfigureWritableWithExplicitPath<SampleOption>(configuration.GetSection("Nested"), AppDomain.CurrentDomain.BaseDirectory);
+        service.ConfigureWritableWithExplicitPath<SampleOption>(configuration.GetSection("Nested"), AppDomain.CurrentDomain.BaseDirectory!);
 
         var provider = service.BuildServiceProvider();
         var option = provider.GetService<IWritableOptions<SampleOption>>();
