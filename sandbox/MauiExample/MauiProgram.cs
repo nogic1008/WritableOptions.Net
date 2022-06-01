fd65c228c5213cb97ca45bcceaa9110c52cb0d47
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-06-01 02:16:29 +0000

    refactor(sample): use method chain

--- sandbox/MauiExample/MauiProgram.cs
+++ sandbox/MauiExample/MauiProgram.cs
@@ -8,15 +8,20 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+
+        // MAUI
         builder
             .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            });
+            .ConfigureFonts(fonts => fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"));
+
+        // Configuration
         builder.Configuration.AddJsonFile("appsettings.json", true, true);
-        builder.Services.ConfigureWritable<AppOption>(builder.Configuration.GetSection(nameof(AppOption)));
-        builder.Services.AddSingleton<MainPage>();
+
+        // DI
+        builder.Services
+            .ConfigureWritable<AppOption>(builder.Configuration.GetSection(nameof(AppOption)))
+            .AddSingleton<MainPage>();
+
         return builder.Build();
     }
 }
