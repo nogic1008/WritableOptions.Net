57e596a66379038458012d0643b81a9f4226c046
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-12-22 10:55:55 +0900

    chore: revert baseline changes

--- sandbox/Benchmark/BenchmarkConfig.cs
+++ sandbox/Benchmark/BenchmarkConfig.cs
@@ -8,9 +8,9 @@ namespace Benchmark;
 public class BenchmarkConfig : ManualConfig
 {
     public BenchmarkConfig()
-        => AddJob(Job.Default.WithRuntime(CoreRuntime.Core60))
+        => AddJob(Job.Default.WithRuntime(CoreRuntime.Core70))
             .WithOptions(ConfigOptions.DisableOptimizationsValidator)
-            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core70))
+            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core60))
             .WithOptions(ConfigOptions.DisableOptimizationsValidator)
             .AddJob(Job.Default.WithRuntime(ClrRuntime.Net48))
             .WithOptions(ConfigOptions.DisableOptimizationsValidator)
