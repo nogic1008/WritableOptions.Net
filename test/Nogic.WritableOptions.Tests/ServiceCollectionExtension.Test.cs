549e1cb72923e4e3f48d514c2555a5948472f2e1
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2024-06-09 10:10:19 +0900

    test: add extension methods test

--- test/Nogic.WritableOptions.Tests/ServiceCollectionExtension.Test.cs
+++ test/Nogic.WritableOptions.Tests/ServiceCollectionExtension.Test.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,7 +11,7 @@ namespace Nogic.WritableOptions.Tests;
 public sealed class ServiceCollectionExtensionTest
 {
     /// <summary>
-    /// <see cref="ServiceCollectionExtension.ConfigureWritable{TOptions}"/> calls <see cref="ServiceCollectionServiceExtensions.AddTransient{IWritableOptions{TOptions}}(IServiceCollection, Func{IServiceProvider, IWritableOptions{TOptions}})"/>.
+    /// <see cref="ServiceCollectionExtension.ConfigureWritable{TOptions}(IServiceCollection, IConfigurationSection, string)"/> calls <see cref="ServiceCollectionServiceExtensions.AddTransient{IWritableOptions{TOptions}}(IServiceCollection, Func{IServiceProvider, IWritableOptions{TOptions}})"/>.
     /// </summary>
     [TestMethod($"{nameof(ServiceCollectionExtension.ConfigureWritable)}<TOptions>() calls {nameof(ServiceCollectionServiceExtensions.AddTransient)}<IWritableOptions<TOptions>>().")]
     public void ConfigureWritable_Calls_AddTransient()
@@ -35,7 +36,58 @@ public sealed class ServiceCollectionExtensionTest
     }
 
     /// <summary>
-    /// <see cref="ServiceCollectionExtension.ConfigureWritableWithExplicitPath{TOptions}"/> calls <see cref="ServiceCollectionServiceExtensions.AddTransient{IWritableOptions{TOptions}}(IServiceCollection, Func{IServiceProvider, IWritableOptions{TOptions}})"/>.
+    /// <see cref="ServiceCollectionExtension.ConfigureWritable{TOptions}(IServiceCollection, IConfigurationSection, JsonSerializerOptions, string)"/> calls <see cref="ServiceCollectionServiceExtensions.AddTransient{IWritableOptions{TOptions}}(IServiceCollection, Func{IServiceProvider, IWritableOptions{TOptions}})"/>.
+    /// </summary>
+    [TestMethod($"{nameof(ServiceCollectionExtension.ConfigureWritable)}<TOptions>({nameof(JsonSerializerOptions)}) calls {nameof(ServiceCollectionServiceExtensions.AddTransient)}<IWritableOptions<TOptions>>().")]
+    public void ConfigureWritable_JsonSerializerOptions_Calls_AddTransient()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection()
+            .Build();
+        var service = new ServiceCollection();
+        var options = new JsonSerializerOptions();
+
+        // Act
+        _ = service.ConfigureWritable<SampleOption>(configuration.GetSection("Nested"), options);
+        var provider = service.BuildServiceProvider();
+
+        // Assert
+        var option = provider.GetService<IWritableOptions<SampleOption>>();
+        var otherOption = provider.GetService<IWritableOptions<SampleOption>>();
+
+        _ = option.Should().NotBeNull()
+            .And.BeOfType<JsonWritableOptions<SampleOption>>()
+            .And.NotBe(otherOption);
+    }
+
+    /// <summary>
+    /// <see cref="ServiceCollectionExtension.ConfigureWritable{TOptions}(IServiceCollection, IConfigurationSection, Func{JsonSerializerOptions}, string)"/> calls <see cref="ServiceCollectionServiceExtensions.AddTransient{IWritableOptions{TOptions}}(IServiceCollection, Func{IServiceProvider, IWritableOptions{TOptions}})"/>.
+    /// </summary>
+    [TestMethod($"{nameof(ServiceCollectionExtension.ConfigureWritable)}<TOptions>({nameof(Func<JsonSerializerOptions>)}) calls {nameof(ServiceCollectionServiceExtensions.AddTransient)}<IWritableOptions<TOptions>>().")]
+    public void ConfigureWritable_FuncJsonSerializerOptions_Calls_AddTransient()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection()
+            .Build();
+        var service = new ServiceCollection();
+
+        // Act
+        _ = service.ConfigureWritable<SampleOption>(configuration.GetSection("Nested"), () => new());
+        var provider = service.BuildServiceProvider();
+
+        // Assert
+        var option = provider.GetService<IWritableOptions<SampleOption>>();
+        var otherOption = provider.GetService<IWritableOptions<SampleOption>>();
+
+        _ = option.Should().NotBeNull()
+            .And.BeOfType<JsonWritableOptions<SampleOption>>()
+            .And.NotBe(otherOption);
+    }
+
+    /// <summary>
+    /// <see cref="ServiceCollectionExtension.ConfigureWritableWithExplicitPath{TOptions}(IServiceCollection, IConfigurationSection, string, string)"/> calls <see cref="ServiceCollectionServiceExtensions.AddTransient{IWritableOptions{TOptions}}(IServiceCollection, Func{IServiceProvider, IWritableOptions{TOptions}})"/>.
     /// </summary>
     [TestMethod($"{nameof(ServiceCollectionExtension.ConfigureWritableWithExplicitPath)}<TOptions>() calls {nameof(ServiceCollectionServiceExtensions.AddTransient)}<IWritableOptions<TOptions>>().")]
     public void ConfigureWritableWithExplicitPath_Calls_AddTransient()
@@ -48,6 +100,57 @@ public sealed class ServiceCollectionExtensionTest
 
         // Act
         _ = service.ConfigureWritableWithExplicitPath<SampleOption>(configuration.GetSection("Nested"), AppDomain.CurrentDomain.BaseDirectory!);
+        var provider = service.BuildServiceProvider();
+
+        // Assert
+        var option = provider.GetService<IWritableOptions<SampleOption>>();
+        var otherOption = provider.GetService<IWritableOptions<SampleOption>>();
+
+        _ = option.Should().NotBeNull()
+            .And.BeOfType<JsonWritableOptions<SampleOption>>()
+            .And.NotBe(otherOption);
+    }
+
+    /// <summary>
+    /// <see cref="ServiceCollectionExtension.ConfigureWritableWithExplicitPath{TOptions}(IServiceCollection, IConfigurationSection, string, JsonSerializerOptions, string)"/> calls <see cref="ServiceCollectionServiceExtensions.AddTransient{IWritableOptions{TOptions}}(IServiceCollection, Func{IServiceProvider, IWritableOptions{TOptions}})"/>.
+    /// </summary>
+    [TestMethod($"{nameof(ServiceCollectionExtension.ConfigureWritableWithExplicitPath)}<TOptions>({nameof(JsonSerializerOptions)}) calls {nameof(ServiceCollectionServiceExtensions.AddTransient)}<IWritableOptions<TOptions>>().")]
+    public void ConfigureWritableWithExplicitPath_JsonSerializerOptions_Calls_AddTransient()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection()
+            .Build();
+        var service = new ServiceCollection();
+        var options = new JsonSerializerOptions();
+
+        // Act
+        _ = service.ConfigureWritableWithExplicitPath<SampleOption>(configuration.GetSection("Nested"), AppDomain.CurrentDomain.BaseDirectory!, options);
+        var provider = service.BuildServiceProvider();
+
+        // Assert
+        var option = provider.GetService<IWritableOptions<SampleOption>>();
+        var otherOption = provider.GetService<IWritableOptions<SampleOption>>();
+
+        _ = option.Should().NotBeNull()
+            .And.BeOfType<JsonWritableOptions<SampleOption>>()
+            .And.NotBe(otherOption);
+    }
+
+    /// <summary>
+    /// <see cref="ServiceCollectionExtension.ConfigureWritableWithExplicitPath{TOptions}(IServiceCollection, IConfigurationSection, string, Func{JsonSerializerOptions}, string)"/> calls <see cref="ServiceCollectionServiceExtensions.AddTransient{IWritableOptions{TOptions}}(IServiceCollection, Func{IServiceProvider, IWritableOptions{TOptions}})"/>.
+    /// </summary>
+    [TestMethod($"{nameof(ServiceCollectionExtension.ConfigureWritableWithExplicitPath)}<TOptions>({nameof(Func<JsonSerializerOptions>)}) calls {nameof(ServiceCollectionServiceExtensions.AddTransient)}<IWritableOptions<TOptions>>().")]
+    public void ConfigureWritableWithExplicitPath_FuncJsonSerializerOptions_Calls_AddTransient()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection()
+            .Build();
+        var service = new ServiceCollection();
+
+        // Act
+        _ = service.ConfigureWritableWithExplicitPath<SampleOption>(configuration.GetSection("Nested"), AppDomain.CurrentDomain.BaseDirectory!, () => new());
         var provider = service.BuildServiceProvider();
 
         // Assert
