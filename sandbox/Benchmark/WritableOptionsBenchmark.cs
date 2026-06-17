676d95fee5f5242c987aad01dfa8b18a4f672607
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2026-06-17 22:19:23 +0900

    chore: use ServiceCollection on benchmark

--- sandbox/Benchmark/WritableOptionsBenchmark.cs
+++ sandbox/Benchmark/WritableOptionsBenchmark.cs
@@ -1,9 +1,12 @@
 #nullable disable
 extern alias dev;
 extern alias release;
+using Awesome.Net.WritableOptions.Extensions;
 using BenchmarkDotNet.Attributes;
-using Microsoft.Extensions.Options;
-using Moq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 using AwesomeNet = Awesome.Net.WritableOptions;
 using Dev = dev::Nogic.WritableOptions;
 using Release = release::Nogic.WritableOptions;
@@ -22,10 +25,12 @@ public class WritableOptionsBenchmark
 
     private const int UpdateLoopCount = 1000;
 
-    private IOptionsMonitor<SampleOption> _options;
-    private AwesomeNet.WritableOptions<SampleOption> _awesomeWritableOptions;
-    private Release.JsonWritableOptions<SampleOption> _releaseWritableOptions;
-    private Dev.JsonWritableOptions<SampleOption> _devWritableOptions;
+    private AwesomeNet.IWritableOptions<SampleOption> _awesomeWritableOptions;
+    private Release.IWritableOptions<SampleOption> _releaseWritableOptions;
+    private Dev.IWritableOptions<SampleOption> _devWritableOptions;
+    private ServiceProvider _awesomeProvider;
+    private ServiceProvider _releaseProvider;
+    private ServiceProvider _devProvider;
     private SampleOption _option;
     private string _jsonFilePath;
 
@@ -46,10 +51,48 @@ public class WritableOptionsBenchmark
     {
         _jsonFilePath = Path.GetTempFileName();
         File.AppendAllText(_jsonFilePath, "{}");
-        _options = new Mock<IOptionsMonitor<SampleOption>>().Object;
-        _awesomeWritableOptions = new(_jsonFilePath, nameof(SampleOption), _options, null);
-        _releaseWritableOptions = new(_jsonFilePath, nameof(SampleOption), _options, null);
-        _devWritableOptions = new(_jsonFilePath, nameof(SampleOption), _options, null);
+
+        string directoryPath = Path.GetDirectoryName(_jsonFilePath)!;
+        string fileName = Path.GetFileName(_jsonFilePath);
+
+        var awesomeConfiguration = CreateConfiguration(directoryPath, fileName);
+        var awesomeServices = new ServiceCollection();
+        awesomeServices.AddSingleton<IConfiguration>(awesomeConfiguration);
+        awesomeServices.AddSingleton<IHostEnvironment>(new BenchmarkHostEnvironment(directoryPath));
+        awesomeServices.ConfigureWritableOptions<SampleOption>(
+            awesomeConfiguration,
+            nameof(SampleOption),
+            fileName
+        );
+        _awesomeProvider = awesomeServices.BuildServiceProvider();
+
+        var releaseConfiguration = CreateConfiguration(directoryPath, fileName);
+        var releaseServices = new ServiceCollection();
+        releaseServices.AddSingleton<IConfiguration>(releaseConfiguration);
+        releaseServices.AddSingleton<IHostEnvironment>(new BenchmarkHostEnvironment(directoryPath));
+        Release.ServiceCollectionExtension.ConfigureWritable<SampleOption>(
+            releaseServices,
+            releaseConfiguration.GetSection(nameof(SampleOption)),
+            fileName
+        );
+        _releaseProvider = releaseServices.BuildServiceProvider();
+
+        var devConfiguration = CreateConfiguration(directoryPath, fileName);
+        var devServices = new ServiceCollection();
+        devServices.AddSingleton<IConfiguration>(devConfiguration);
+        devServices.AddSingleton<IHostEnvironment>(new BenchmarkHostEnvironment(directoryPath));
+        Dev.ServiceCollectionExtension.ConfigureWritable<SampleOption>(
+            devServices,
+            devConfiguration.GetSection(nameof(SampleOption)),
+            fileName
+        );
+        _devProvider = devServices.BuildServiceProvider();
+
+        _awesomeWritableOptions =
+            _awesomeProvider.GetRequiredService<AwesomeNet.IWritableOptions<SampleOption>>();
+        _releaseWritableOptions =
+            _releaseProvider.GetRequiredService<Release.IWritableOptions<SampleOption>>();
+        _devWritableOptions = _devProvider.GetRequiredService<Dev.IWritableOptions<SampleOption>>();
     }
 
     [Benchmark(
@@ -88,7 +131,26 @@ public class WritableOptionsBenchmark
     [IterationCleanup]
     public void Teardown()
     {
+        _awesomeProvider?.Dispose();
+        _releaseProvider?.Dispose();
+        _devProvider?.Dispose();
+
         if (File.Exists(_jsonFilePath))
             File.Delete(_jsonFilePath);
+    }
+
+    private static IConfigurationRoot CreateConfiguration(string directoryPath, string fileName) =>
+        new ConfigurationBuilder().SetBasePath(directoryPath).AddJsonFile(fileName).Build();
+
+    private sealed class BenchmarkHostEnvironment(string contentRootPath) : IHostEnvironment
+    {
+        public string EnvironmentName { get; set; } = "Benchmark";
+
+        public string ApplicationName { get; set; } = nameof(WritableOptionsBenchmark);
+
+        public string ContentRootPath { get; set; } = contentRootPath;
+
+        public IFileProvider ContentRootFileProvider { get; set; } =
+            new PhysicalFileProvider(contentRootPath);
     }
 }
